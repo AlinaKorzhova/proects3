cee886f8f42a90ee03c8598ee0786719a098f496
Alina Korzhova <alina.solod1997@yandex.ru>
2026-08-03 19:29:23 +0300

    init func

---
+++ src/processing.py
@@ -0,0 +1,24 @@
+def filter_by_state(transactions: list, state: str = "EXECUTED") -> list:
+    """фильтруем по "state" """
+    result = []
+    for num in transactions:
+        if num["state"] == state:
+            result.append(num)
+    return result
+
+
+def sort_by_date(transactions: list) -> list:
+    """функция сортирует по дате"""
+    sort_func = sorted(transactions, key=lambda x: x["date"], reverse=True)
+    return sort_func
+
+
+if __name__ == "__main__":
+    transactions = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+    print(filter_by_state(transactions))
+    print(sort_by_date(transactions))
